165ce8ea868b154b57b530bf4f55347ee823e682
Alejandro <alejandrosilveiraramos3@gmail.com>
2022-09-08 21:57:37 -0300

    Calculator while

--- classes/module02/class03.py
+++ classes/module02/class03.py
@@ -2,19 +2,12 @@
 
 num = float(input("Insert any number to show in multiplicatin table: "))
 
-print(
-    " {} X {} = {} \n {} X {} = {} \n {} X {} = {} \n {} X {} = {} \n {} X {} = {} \n {} X {} = {} \n {} X {} = {} \n {} X {} = {} \n {} X {} = {} \n {} X {} = {} \n"
-    .format(
-        num, 1, num*1,
-        num, 2, num*2,
-        num, 3, num*3,
-        num, 4, num*4,
-        num, 5, num*5,
-        num, 6, num*6,
-        num, 7, num*7,
-        num, 8, num*8,
-        num, 9, num*9,
-        num, 10, num*10
-        ) 
-)
+contador = 0
 
+while contador < 9:
+    print(" {} X {} = {} ".format( num, contador, num*contador ))
+
+    contador += 1
+else:
+    print("Acabou")
+    
