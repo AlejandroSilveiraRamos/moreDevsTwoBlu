5068fecd64b603b8271fc11fe6d934048fe203b8
Alejandro <alejandrosilveiraramos3@gmail.com>
2022-09-12 18:32:52 -0300

    feat: class 09/09/22

---
+++ classes/module03/classOP01.py
@@ -0,0 +1,53 @@
+validatorStuff = False
+print(validatorStuff)
+#--- Trade initial value
+validatorStuff = True
+print(validatorStuff)
+
+age = int(input("Your age: "))
+
+print(" "*15, "Equal expression", " "*15, "\n")
+#--- Creating boolean variable from equal expression
+age = 18
+validatorStuff  = (age == 18)
+print(validatorStuff)
+
+print(" "*15, "Different expression", " "*15, "\n")
+
+#--- Creating boolean variable from different expression
+validatorStuff  = (age != 18)
+print(validatorStuff)
+
+print(" "*15, "Different expression", " "*15, "\n")
+
+#--- Creating boolean variable from bigger expression
+validatorStuff  = (age > 18)
+print(validatorStuff)
+
+print(" "*15, "Different expression", " "*15, "\n")
+
+#--- Creating boolean variable from smaller expression
+validatorStuff  = (age < 18)
+print(validatorStuff)
+
+print(" "*15, "Different expression", " "*15, "\n")
+
+#--- Creating boolean variable from bigger or equal expression
+validatorStuff  = (age >= 18)
+print(validatorStuff)
+
+print(" "*15, "Different expression", " "*15, "\n")
+
+#--- Creating boolean variable from smaller or equal expression
+validatorStuff  = (age <= 18)
+print(validatorStuff)
+
+print(" "*15, "Different expression", " "*15, "\n")
+
+
+
+
+
+
+
+
